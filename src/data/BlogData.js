7eef4102aad54379764706e077aea2f3ccbe8fb2
Rahul Rawat <rahul.rawat.fec@gmail.com>
2022-02-18 21:18:13 +0530

    code pushed

---
+++ src/data/BlogData.js
@@ -0,0 +1,78 @@
+// blog data
+import imgEcart from "../assets/Images/Ecart.PNG"
+import Netflix from "../assets/Images/Netflix.PNG"
+import goodspace from "../assets/Images/goodspace.PNG"
+import Portfolio from "../assets/Images/Portfolio.PNG"
+import TransparentForm from "../assets/Images/TransparentForm.PNG"
+import MultiselectForm from "../assets/Images/MultiselectForm.PNG"
+import survey from "../assets/Images/survey.PNG"
+import OWl from "../assets/Images/OWl.PNG"
+export const Blogs = [
+    {
+        id:1,
+        name:"A Netflix Clone in ReactJs and CSS for Styling ",
+        tags:["reactjs","CSS"],
+        date:"5 July, 2021",
+        imgSrc:Netflix,
+        link:"https://rahul-9211.github.io/Netflix"
+    },
+    {
+        id:2,
+        name:"A complete Portfolio for Web Designer or Developer using HTML , CSS and JavaScript",
+        tags:["javascript","HTML","CSS"],
+        date:"6 October, 2020",
+        imgSrc:Portfolio,
+        link:"https://rahul-9211.github.io/Portfolio"
+    },
+    {
+        id:3,
+        name:"Build Website with ReactJS,ExpressJs , NodeJs and MongoDB for Database",
+        tags:["reactjs","expressjs","nodejs", "mongoDB"],
+        date:"13 March, 2021",
+        imgSrc:imgEcart,
+        link:"https://rahul-9211.github.io/ECART_REACT"
+    },
+    {
+        id:4,
+        name:"A simple static dashboard Template for goodspace ",
+        tags:["HTML","CSS"],
+        date:"26 january, 2021",
+        imgSrc:goodspace,
+        link:"https://rahul-9211.github.io/goodspace"
+    },
+    {
+        id:5,
+        name:"A Transparent Form with Login auth.",
+        tags:["CSS","HTML","web-development" ,  "javascript"],
+        date:"3 Feb, 2021",
+        imgSrc:TransparentForm,
+        link:"https://rahul-9211.github.io/Transparent_login_form/"
+    },
+    {
+        id:6,
+        name:"A Multiselect option Form ",
+        tags:["CSS","HTML","web-development" ,  "javascript"],
+        date:"3 Feb, 2021",
+        imgSrc:MultiselectForm,
+        link:"https://rahul-9211.github.io/Multiselect-form/"
+
+    },
+   
+    {
+        id:7,
+        name:"A night own template website using frame in JS , HTML and CSS for styling .",
+        tags:["javascript","CSS","Html"],
+        date:"16 May, 2021",
+        imgSrc:OWl,
+        link:"https://rahul-9211.github.io/owlvpn/"
+    },
+    {
+        id:8,
+        name:"A survey data analysis project using Machine Learning with the help of python",
+        tags:["numpy","pandas","pyhton"],
+        date:"26 june, 2019",
+        imgSrc:survey,
+        link:"https://github.com/Rahul-9211/Survey_data_analytics"
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,39 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Xiarch Solution Private Limited",
+        description:"Currently I am working as a Trainee in Xiarch Solutions Private Limited . My designation is Software Developer || Full Stack Developer",
+        tags:["MERN","software","developer" , "currently"],
+        demo:"https://www.xiarch.com/",
+        github:"https://in.linkedin.com/company/xiarch"
+    },
+    {
+        id:2,
+        name:"Degpeg Media and Technologies Private Limited",
+        description:"I did Internship of 2-3 Months from Degpeg Media and Technologies Private Limited . My role was Web Developer over there",
+        tags:["MERN","software","developer" , "internship"],
+        demo:"https://www.degpeg.com/",
+        github:"https://in.linkedin.com/company/degpeg",
+        
+    },
+    {
+        id:3,
+        name:"Learning Brothers Private Limited",
+        description:"I did Internship of 6 Months from Degpeg Media and Technologies Private Limited . My role was Web Developer over there .",
+        tags:["MERN","software","developer" , "internship"],
+        demo:"https://www.learningbrothers.com/",
+        github:"https://in.linkedin.com/company/degpeg",
+    },
+    {
+        id:4,
+        name:"Codec Networks",
+        description:"I did training of Data Science Engineer from Codec Networks. Using technologies of Machine Learning ",
+        tags:["python","machineLearning","numpy" , "pandas"],
+        demo:"https://www.codecnetworks.com/",
+        github:"https://in.linkedin.com/company/codec-networks-trainings"
+       
+    }
+  
+] 
